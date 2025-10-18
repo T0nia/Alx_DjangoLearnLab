f87a18fad0d6dd1f0e96122041a823f0ff33feeb
T0nia <edtoniaelohor@gmail.com>
2025-10-18 23:35:35 +0100

    new views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,3 +11,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns += [
+    path('admin-role/', admin_view, name='admin_view'),
+    path('librarian-role/', librarian_view, name='librarian_view'),
+    path('member-role/', member_view, name='member_view'),
+]
