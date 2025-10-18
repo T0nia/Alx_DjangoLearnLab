f799dba654eead615c67a865d7e1ce2fcb79c306
T0nia <edtoniaelohor@gmail.com>
2025-10-18 23:03:47 +0100

    fixed query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,17 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author (example: "George Orwell")
-orwell = Author.objects.get(name="George Orwell")
-books_by_orwell = Book.objects.filter(author=orwell)
-print("Books by George Orwell:", books_by_orwell)
+# 1. Query all books by a specific author
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print("Books by", author_name, ":", books_by_author)
 
-# 2. List all books in a library (example: "Central Library")
-library = Library.objects.get(name="Central Library")
+# 2. List all books in a library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print("Books in Central Library:", books_in_library)
+print("Books in", library_name, ":", books_in_library)
 
-# 3. Retrieve the librarian for a library (example: "Central Library")
+# 3. Retrieve the librarian for a library
 librarian = Librarian.objects.get(library=library)
-print("Librarian of Central Library:", librarian)
+print("Librarian of", library_name, ":", librarian)
